616d14224cee70e8f16238f162eded0c3247fa0a
edmekit <mekitpekit.eggmarc@gmail.com>
2026-05-28 16:53:31 +0800

    added some effects

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ fetch("char.json")
 .then(response => response.json())
 .then(data => {
 fur.addEventListener("click", () => {
-    display.innerHTML = "Name: " + data.furina.name + "<br>Element: " 
+    display.innerHTML = "<img src = 'images/furina.jpg'>Name: " + data.furina.name + "<br>Element: " 
     + data.furina.element + "<br> Recommended: <br> " + "Weapon: " + data.furina.bestweapon + "<br> F2P: " + data.furina.f2p + "<br>Aim for: <br> HP: " + data.furina.hp + "<br> Crit Damage: " + data.furina.critdmg + "<br> Crit Rate: " + data.furina.critrt
 });
 cha.addEventListener("click", () => {
-    display.innerHTML = "Name: " + data.chasca.name + "<br>Element: " 
+    display.innerHTML = "<img src = 'images/chasca.png'>Name: " + data.chasca.name + "<br>Element: " 
     + data.chasca.element + "<br> Recommended: <br> " + "Weapon: " + data.chasca.bestweapon + "<br> F2P: " + data.chasca.f2p + "<br>Aim for: <br> ATK: " + data.chasca.atk + "<br> Crit Damage: " + data.chasca.critdmg + "<br> Crit Rate: " + data.chasca.critrt
 });
 zib.addEventListener("click", () => {
-    display.innerHTML = "Name: " + data.zibai.name + "<br>Element: "
+    display.innerHTML = "<img src = 'images/zibai.jpg'> Name: " + data.zibai.name + "<br>Element: "
     + data.zibai.element + "<br> Recommended: <br> " + "Weapon: " + data.zibai.bestweapon + "<br> F2P: " + data.zibai.f2p + "<br>Aim for: <br> DEF: " + data.zibai.def + "<br> Crit Damage: " + data.zibai.critdmg + "<br> Crit Rate: " + data.zibai.critrt
 });
 })
