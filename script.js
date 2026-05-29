e508100fdc5694b665dd19038836b30c1b89ed1f
edmekit <mekitpekit.eggmarc@gmail.com>
2026-05-29 19:12:34 +0800

    better ui

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const fur = document.getElementById("furinabtn");
 const cha = document.getElementById("chascabtn");
 const zib = document.getElementById("zibaibtn");
-let display = document.getElementById("char");
+let info = document.getElementById("charinfo");
+let image = document.getElementById("charimg");
+let stats = document.getElementById("stats");
 
 
 fetch("char.json")
 .then(response => response.json())
 .then(data => {
 fur.addEventListener("click", () => {
-    display.innerHTML = "<img src = 'images/furina.jpg'>Name: " + data.furina.name + "<br>Element: " 
-    + data.furina.element + "<br> Recommended: <br> " + "Weapon: " + data.furina.bestweapon + "<br> F2P: " + data.furina.f2p + "<br>Aim for: <br> HP: " + data.furina.hp + "<br> Crit Damage: " + data.furina.critdmg + "<br> Crit Rate: " + data.furina.critrt
+    showChar(data.furina);
 });
 cha.addEventListener("click", () => {
-    display.innerHTML = "<img src = 'images/chasca.png'>Name: " + data.chasca.name + "<br>Element: " 
-    + data.chasca.element + "<br> Recommended: <br> " + "Weapon: " + data.chasca.bestweapon + "<br> F2P: " + data.chasca.f2p + "<br>Aim for: <br> ATK: " + data.chasca.atk + "<br> Crit Damage: " + data.chasca.critdmg + "<br> Crit Rate: " + data.chasca.critrt
+    showChar(data.chasca);
 });
 zib.addEventListener("click", () => {
-    display.innerHTML = "<img src = 'images/zibai.jpg'> Name: " + data.zibai.name + "<br>Element: "
-    + data.zibai.element + "<br> Recommended: <br> " + "Weapon: " + data.zibai.bestweapon + "<br> F2P: " + data.zibai.f2p + "<br>Aim for: <br> DEF: " + data.zibai.def + "<br> Crit Damage: " + data.zibai.critdmg + "<br> Crit Rate: " + data.zibai.critrt
+    showChar(data.zibai);
 });
 })
 .catch(error => console.error("JSON load failed:", error));
+
+function showChar(chardata){
+
+    stats.classList.add("open");
+    image.innerHTML = `<img src = "${chardata.image}">`;
+    info.innerHTML = `
+    Name: ${chardata.name}<br>
+    Element: ${chardata.element}<br> 
+    Recommended: <br>
+    Weapon: ${chardata.bestweapon}<br> 
+    F2P: ${chardata.f2p}<br>`
+    
+}
