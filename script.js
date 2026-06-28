32201805ec336985961403bd01347936ed083030
edmekit <mekitpekit.eggmarc@gmail.com>
2026-06-28 19:17:12 +0800

    wow API

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-const fur = document.getElementById("furinabtn");
-const cha = document.getElementById("chascabtn");
-const zib = document.getElementById("zibaibtn");
-let info = document.getElementById("charinfo");
-let image = document.getElementById("charimg");
-let stats = document.getElementById("stats");
+const search = document.getElementById("searchbtn");
+const stats = document.getElementById("stats");
+const info = document.getElementById("charinfo");
+
+search.addEventListener("click", function() {
+    const searchbar = document.getElementById("searchbar").value.trim();
+    const url = `https://gsi.fly.dev/characters/search?name=${searchbar}`;
+
+        fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        showChar(data.results[0]);
+    })
+    .catch(error => console.error("JSON load failed:", error));
+});
 
 
-fetch("char.json")
-.then(response => response.json())
-.then(data => {
-fur.addEventListener("click", () => {
-    showChar(data.furina);
-});
-cha.addEventListener("click", () => {
-    showChar(data.chasca);
-});
-zib.addEventListener("click", () => {
-    showChar(data.zibai);
-});
-})
-.catch(error => console.error("JSON load failed:", error));
 
 function showChar(chardata){
 
     stats.classList.add("open");
-    image.innerHTML = `<img src = "${chardata.image}">`;
     info.innerHTML = `
     Name: ${chardata.name}<br>
-    Element: ${chardata.element}<br> 
+    Element: ${chardata.vision}<br> 
     Recommended: <br>
-    Weapon: ${chardata.bestweapon}<br> 
-    F2P: ${chardata.f2p}<br>`
+    Weapon: ${chardata.weapon}<br> 
+    F2P: ${chardata.rarity}<br>`
     
 }
