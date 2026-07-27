4528fe4e4d98e9bac9bb91822eb10d6ac20d1485
edmekit <mekitpekit.eggmarc@gmail.com>
2026-07-27 22:11:09 +0800

    added skeleton

--- gsi-char-card/src/LoadingCard.jsx
+++ gsi-char-card/src/LoadingCard.jsx
@@ -1,4 +1,18 @@
+import { motion } from 'framer-motion'
+
+
 function LoadingCard(){
+    const skeletonVariant = {
+        loading: {
+            scale: [1, 1.1, 1],
+            transition: {
+            duration: 1.2,
+            repeat: Infinity,
+            ease: "easeInOut",
+            },
+        },
+    };
+
     return (
         <div className="load-card"
         style={{backgroundImage: `
@@ -7,8 +21,30 @@ function LoadingCard(){
             rgba(0,0,0,0.5),
             rgba(0,0,0,0.2)
             )`}}>
-                <h1>Generating Card...</h1>
+            <div className="weapon-team">
+                <motion.div className="weapon-loading"
+                variants={skeletonVariant}
+                animate="loading"></motion.div>
+                <motion.div className="team-loading"
+                variants={skeletonVariant} animate="loading"></motion.div>
             </div>
+            <div className="stats-notes">
+                <div className="artifact-stats">
+                    <motion.div className="artifact-loading"
+                    variants={skeletonVariant} animate="loading"></motion.div>
+                    <motion.div className="stats-loading"
+                    variants={skeletonVariant} animate="loading"></motion.div>
+                </div>
+                <motion.div className="notes-loading"
+                variants={skeletonVariant} animate="loading"></motion.div>
+            </div>
+            <div className="char-info">
+                <motion.div className="char-name-loading"
+                variants={skeletonVariant} animate="loading"></motion.div>
+            </div>
+
+
+        </div>
     );
 }
 
